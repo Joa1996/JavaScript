420f7fe3ada21c7ba832f779b776f1f8aaf3f9b8
Joa1996 <joaquingallardo096@gmail.com>
2024-06-23 19:40:24 -0300

    der

--- Arrays2.js
+++ Arrays2.js
@@ -12,8 +12,15 @@ const employees = [
         name:'Maria'
     }
 ];
-
+//Esta funcion buscara todos los elementos del objeto
 const employee =employees.find(function(e) {
-    return e.name === 'Marcos'
+    return e.name === 'Marcos'//Dentor del metodo find(), usamos otra funcion que es del tipo callback y lo que hara sera por cada objeto ejecutar una busqueda
 });
+
+function search(e) {
+    console.log(e);
+    return e.name === 'Marcos'//Dentor del metodo find(), usamos otra funcion que es del tipo callback y lo que hara sera por cada objeto ejecutar una busqueda
+    
+}
+search(employees);
 console.log(employee);
